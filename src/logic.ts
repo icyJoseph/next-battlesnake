2eaff0c4801a8bbb88c9169020a4886b0361c014
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-11-23 14:42:25 +0100

    feat: Attempt to stay on the board

--- src/logic.ts
+++ src/logic.ts
@@ -102,8 +102,10 @@ export function end(gameState: GameState): void {
   console.log(`${gameState.game.id} END\n`);
 }
 
+type Moves = Record<"up" | "down" | "left" | "right", boolean>;
+
 export function move(gameState: GameState): MoveResponse {
-  let possibleMoves: { [key: string]: boolean } = {
+  let possibleMoves: Moves = {
     up: true,
     down: true,
     left: true,
@@ -113,6 +115,7 @@ export function move(gameState: GameState): MoveResponse {
   // Step 0: Don't let your Battlesnake move back on it's own neck
   const myHead = gameState.you.head;
   const myNeck = gameState.you.body[1];
+
   if (myNeck.x < myHead.x) {
     possibleMoves.left = false;
   } else if (myNeck.x > myHead.x) {
@@ -125,8 +128,27 @@ export function move(gameState: GameState): MoveResponse {
 
   // TODO: Step 1 - Don't hit walls.
   // Use information in gameState to prevent your Battlesnake from moving beyond the boundaries of the board.
-  // const boardWidth = gameState.board.width
-  // const boardHeight = gameState.board.height
+  const boardWidth = gameState.board.width;
+  const boardHeight = gameState.board.height;
+
+  // if at the top edge
+  if (myHead.y === 0) {
+    possibleMoves.up = false;
+  }
+  // if at the bottom edge
+  if (myHead.y === boardHeight - 1) {
+    possibleMoves.down = false;
+  }
+
+  // if at the left edge
+  if (myHead.x === 0) {
+    possibleMoves.left = false;
+  }
+
+  // if at the right edge
+  if (myHead.x === boardWidth - 1) {
+    possibleMoves.right = false;
+  }
 
   // TODO: Step 2 - Don't hit yourself.
   // Use information in gameState to prevent your Battlesnake from colliding with itself.
@@ -141,7 +163,7 @@ export function move(gameState: GameState): MoveResponse {
   // Finally, choose a move from the available safe moves.
   // TODO: Step 5 - Select a move to make based on strategy, rather than random.
   const safeMoves = Object.keys(possibleMoves).filter(
-    (key) => possibleMoves[key]
+    (key) => possibleMoves[key as keyof Moves]
   );
   const response: MoveResponse = {
     move: safeMoves[Math.floor(Math.random() * safeMoves.length)]
