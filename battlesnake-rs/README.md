fa3732e61006d16d8e7e59afbbbf1eebcc59c1cc
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-11-24 13:15:43 +0100

    feat: Prepare to publish battlesnake-rs

---
+++ battlesnake-rs/README.md
@@ -0,0 +1,10 @@
+# Battlesnake with Rust
+
+- Compiles to WASM
+- Published to NPM
+
+## Changes
+
+### Version 0.0.0
+
+- Includes only `get_info` function.
--- src/pages/api/wasm/index.js
+++ src/pages/api/wasm/index.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   if (req.method !== "GET") return res.status(404).json({ error: "No" });
 
   try {
-    const wasm = await import("../../../../snake-rs/pkg");
+    const wasm = await import("../../../../battlesnake-rs/pkg");
 
     return res.status(200).json(wasm.get_info());
   } catch (e) {
