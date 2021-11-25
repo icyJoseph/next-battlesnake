159edcbc8bf6c4365b180a447872ec82a0c61709
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-11-25 23:20:32 +0100

    feat: Rank possible moves, and keep only the best

--- src/logic/move.ts
+++ src/logic/move.ts
@@ -7,17 +7,20 @@ import {
   createMatrix,
   quick_bfs
 } from "helpers/bfs";
+import { Dir } from "fs";
 
-type Moves = Record<Directions, boolean>;
+type Moves = Record<Directions, number>;
 
-const isKey = (key: any, moves: Moves): key is Directions => key in moves;
+const directions: Directions[] = ["up", "down", "right", "left"];
+const isKey = (key: any): key is Directions =>
+  Boolean(directions.find((x) => x === key));
 
 export function move(gameState: GameState): MoveResponse {
   let possibleMoves: Moves = {
-    up: false,
-    down: false,
-    left: false,
-    right: false
+    up: Infinity,
+    down: Infinity,
+    left: Infinity,
+    right: Infinity
   };
 
   const { board, you } = gameState;
@@ -80,10 +83,14 @@ export function move(gameState: GameState): MoveResponse {
       // on the same X axis
       if (myHead.y + 1 === y0) {
         // moving down but it is inverted
-        possibleMoves.up = true;
+        possibleMoves.up = adj[val].filter(
+          (val) => distances[val] !== Infinity
+        ).length;
       } else {
         // moving up but it is inverted
-        possibleMoves.down = true;
+        possibleMoves.down = adj[val].filter(
+          (val) => distances[val] !== Infinity
+        ).length;
       }
     }
 
@@ -91,10 +98,14 @@ export function move(gameState: GameState): MoveResponse {
       // on the same Y axis
       if (myHead.x + 1 === x0) {
         // moving to the right
-        possibleMoves.right = true;
+        possibleMoves.right = adj[val].filter(
+          (val) => distances[val] !== Infinity
+        ).length;
       } else {
         // moving to the left
-        possibleMoves.left = true;
+        possibleMoves.left = adj[val].filter(
+          (val) => distances[val] !== Infinity
+        ).length;
       }
     }
   }
@@ -123,12 +134,33 @@ export function move(gameState: GameState): MoveResponse {
 
   // Finally, choose a move from the available safe moves.
   // TODO: Step 5 - Select a move to make based on strategy, rather than random.
-  const safeMoves = Object.keys(possibleMoves).filter(
-    (key): key is keyof Moves => isKey(key, possibleMoves) && possibleMoves[key]
+
+  const absoluteBest = Math.max(
+    ...Object.values(possibleMoves).filter((val) => val !== Infinity)
   );
 
+  const bestMoves: Directions[] = Object.entries(possibleMoves)
+    .filter(([, value]) => value !== Infinity) // remove impossible
+    .sort((a, b) => b[1] - a[1]) // sort ascending
+    .filter(([, value]) => value < absoluteBest) // keep the best
+    .map(([key]) => key) // take only the move name
+    .filter(isKey); // keep TS happy
+
+  if (bestMoves.length === 0) {
+    // HALT AND CATCH FIRE
+    const response: MoveResponse = {
+      move: "up"
+    };
+
+    console.log(
+      `HALT: ${gameState.game.id} MOVE ${gameState.turn}: ${response.move}`
+    );
+    return response;
+  }
+
+  // keep it classy with a random choice
   const response: MoveResponse = {
-    move: safeMoves[Math.floor(Math.random() * safeMoves.length)] || "up"
+    move: bestMoves[Math.floor(Math.random() * bestMoves.length)]
   };
 
   console.log(`${gameState.game.id} MOVE ${gameState.turn}: ${response.move}`);
